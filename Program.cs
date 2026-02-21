6d2a05f906961c5440715bf94f028f8429a8dd68
John Carlo Nayan <nayanjohncarlo123@gmail.com>
2026-02-21 17:11:53 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace EmployeeLeaveManagement
+﻿using System;
+
+namespace EmployeeLeaveManagement
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] LeaveTypes = {"Vacation", "Sick", "Emergency"};
+
+            Console.WriteLine("Welcome to Employee Leave Management System");
+            Console.WriteLine("Please enter your employee ID:");
+            Console.ReadLine();
+
+            Console.WriteLine("Types: Vacation , Sick, Emergency");
+            Console.WriteLine("Please select a leave type:");
+            
+
+            for (int i = 0; i < LeaveTypes.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {LeaveTypes[i]}");
+            }
         }
     }
 }
