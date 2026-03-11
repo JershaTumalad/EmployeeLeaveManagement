5068e9dea2ba5cff95a75b4edac75192270401d1
JershaTumalad <tahtianajershatumalad@gmail.com>
2026-03-11 22:35:48 +0800

    add LeaveService class

---
+++ LeaveService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeLeaveManagement
+{
+    public class LeaveService
+    {
+        public bool HasSufficientPoints(int currentPoints, int requiredPoints)
+        {
+            return currentPoints >= requiredPoints;
+        }
+
+        public bool IsValidDate(DateTime date)
+        {
+            return date >= DateTime.Today;
+        }
+    }
+}
